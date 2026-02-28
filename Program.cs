5a7ac8fd89a880b95f5dadd63fcaa9c1975133b0
MohmmedElshiwy <mohammed6elshiwy@gmail.com>
2026-02-28 16:23:09 +0300

    Problem 1 solutions

--- Program.cs
+++ Program.cs
@@ -1,1 +1,45 @@
 ﻿
+#region Part 1
+    
+#region Problem 1
+
+
+// public class BanckAccount
+// {
+    // public string Owner;
+    // public double Balance;
+    // public void withdraw(double amunt)
+    // {
+    //     Balance -= amunt;
+    // }
+
+// Identify at least two problems with this design from an encapsulation perspective
+
+// 1- the fields are public, this means that any code can access and ,modify the balance and owner
+// 2- the withdraw method does't have any validation 
+
+// To Solve These Problems , we can make The Fields Private 
+
+// private string Owner;
+// private double Balance;
+
+// public void withdraw(double amount)
+//     {
+//         if( amount >Balance){
+//             Console.WriteLine("Insufficient Funds");
+//         }
+//         else{
+//             Balance -= amount;
+//         }
+//     }
+
+// }
+    
+
+    //Explain why exposing fields directly (as public) is considered a bad practice in OOP.
+    //because it breaks Encapulation, it allows any code to access and modify the fields without any control or vaildation
+#endregion
+
+
+
+#endregion
